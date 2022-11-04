8294e168cd77b39064efbf6d728777804932829c
Vyalov Vadim <vyalov.vadim@gmail.com>
2022-11-04 03:53:52 +0200

    Fix 01

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,7 @@
 (() => {
     const menuBtnRef = document.querySelector("[data-menu-button]");
     const mobileMenuRef = document.querySelector("[data-menu]");
+  const htmlRef = document.querySelector("[data-html]");
   
     menuBtnRef.addEventListener("click", () => {
       const expanded =
@@ -8,7 +9,7 @@
   
       menuBtnRef.classList.toggle("is-open");
       menuBtnRef.setAttribute("aria-expanded", !expanded);
-  
       mobileMenuRef.classList.toggle("is-open");
+      htmlRef.classList.toggle("no-scroll");
     });
   })();
